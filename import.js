b6771e2ff2db294c5cd6ddf115ef30b4895d58ce
Daniel Andrade <daniel.gcecia@gmail.com>
2025-12-09 01:21:26 -0300

    Railway

--- import.js
+++ import.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const mysql = require('mysql2');
 
-const db = mysql.createPool(process.env.MYSQL_URL);
+const db = mysql.createPool({
+  host: 'crossover.proxy.rlwy.net',
+  user: 'root',
+  password: 'rjsEdPfyBOxQShDLWepsENaQnQeQaJeo',
+  database: 'railway',
+  port: 46028,
+  multipleStatements: true
+});
 
 const sql = fs.readFileSync('./scripts/script.sql', 'utf8');
+const cleanedSql = sql.replace(/CREATE DATABASE.*;/gi, '').replace(/USE.*;/gi, '');
 
-db.query(sql, (err, results) => {
+db.query(cleanedSql, (err, results) => {
   if (err) console.error('Erro ao importar banco:', err);
   else console.log('Banco importado com sucesso!');
   process.exit();
